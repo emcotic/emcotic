683d8c6c701d0e4dc301d96f6b762927638ba8b0
emcotic <jarangoc@emcotic.com>
2025-11-16 18:14:33 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+// tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme'); // Importamos el tema por defecto
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Aquí definimos la nueva fuente
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      // ESTA ES LA PARTE QUE FALTA:
+      colors: {
+        'emcotic-blue': '#002D62', // Un azul marino ejecutivo
+        'emcotic-cyan': '#00b5e2', // Un cian vibrante para acentos
+      }
+    },
   },
   plugins: [],
 }
