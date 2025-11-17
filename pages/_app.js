c1646f6cc6f3187c81e8c47e05606c14cb6cf4f1
emcotic <jarangoc@emcotic.com>
2025-11-16 19:22:49 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 // /pages/_app.js
 
-// Importamos los estilos globales de Tailwind
 import '../styles/globals.css';
+import Header from '../components/Header'; // Importamos el Header
+import CookieBanner from '../components/CookieBanner'; // Importamos el Banner
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header /> {/* El Header siempre estará arriba */}
+      
+      <Component {...pageProps} /> {/* Aquí se carga la página (index.js, etc.) */}
+      
+      <CookieBanner /> {/* El banner de cookies siempre estará abajo */}
+    </>
+  );
 }
 
 export default MyApp;
